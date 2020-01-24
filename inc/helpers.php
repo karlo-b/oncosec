bb84a71699115ae000845e52f98bea7630d10c66
Karlo <karlo1405@gmail.com>
2020-01-24 11:28:06 +0100

    cleaning

--- inc/helpers.php
+++ inc/helpers.php
@@ -1181,3 +1181,18 @@ function wpb_remove_version() {
 	return '';
 }
 	add_filter( 'the_generator', 'wpb_remove_version' );
+
+
+
+// Image helper function
+function wp_get_attachment( $attachment_id ) {
+	$attachment = get_post( $attachment_id );
+	return array(
+		'alt'         => get_post_meta( $attachment->ID, '_wp_attachment_image_alt', true ),
+		'caption'     => $attachment->post_excerpt,
+		'description' => $attachment->post_content,
+		'href'        => get_permalink( $attachment->ID ),
+		'src'         => $attachment->guid,
+		'title'       => $attachment->post_title,
+	);
+}
